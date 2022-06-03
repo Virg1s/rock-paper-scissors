9759fb44983189be759878aaee642de067ceef0d
Virg1s <virginijusruibys@gmail.com>
2022-06-03 11:32:21 +0300

    finished the game

--- app/controllers/throw_controller.rb
+++ app/controllers/throw_controller.rb
@@ -6,12 +6,37 @@ class ThrowController < ApplicationController
             'production' => 'https://5eddt4q9dk.execute-api.us-east-1.amazonaws.com/rps-stage/throw' }.freeze
   RULES = { 'rock' => ['scissors'], 'paper' => ['rock'], 'scissors' => ['paper'] }.freeze
 
+  ## Rules can be modified to include additional signs by adding additional k:v pairs, where k is new sign and v is array of signs weaker then the new sign
+  ## For example uncomment the following line to include hammer:
+  #RULES = { 'rock' => ['scissors'], 'paper' => ['rock'], 'scissors' => ['paper'], 'hammer' => ['rock', 'scissors'] }.freeze
+
   def index
     @bets = RULES.keys
 
     @game_name = @bets.map(&:upcase).join('-')
 
     @rule_description = RULES.map { |key, value| "- #{key.camelize} beats #{value.join(', ')}" }
+  end
+
+  def result
+    @heading, @message = outcome(user_choice: params[:throw], server_choice: get_server_choice)
+  end
+
+  def crub_busy
+  end
+
+  private
+
+  def outcome(user_choice:, server_choice:)
+    choices = "You chose #{user_choice} and Crub chose #{server_choice}"
+
+    if user_choice == server_choice
+      return ['Draw!', "Both you and Crub chose #{user_choice}"]
+    elsif RULES[user_choice].include? server_choice
+      return ['You won!', choices]
+    else
+      return ['You lost!', choices]
+    end
   end
 
   def get_server_choice
@@ -32,8 +57,5 @@ class ThrowController < ApplicationController
     redirect_to crub_busy_path unless status_code_good && body_good
 
     result['body']
-  end
-
-  def result
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'throw', to: "throw#index"
   get 'result', to: "throw#result"
+  get 'crub_busy', to: "throw#crub_busy"
 end
